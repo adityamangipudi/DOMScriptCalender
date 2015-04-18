c60246c9b0f147d025399b404cd467d418d352ea
adityamangipudi <aditya.mangipudi@gmail.com>
2015-04-18 01:27:05 -0700

    Promts user for appointment

--- Calendar.js
+++ Calendar.js
@@ -1,80 +1,155 @@
 
 
-    document.addEventListener('DOMContentLoaded', function () {
-        var hours= ['12', '12:30', '1']
-        function createDaysArr(){
+document.addEventListener('DOMContentLoaded', function () {
+    var month = [];
+    var hours= ['12', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+    var months = [ "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December" ];
 
-            var month = [];
-            var months = [ "January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December" ];
-
-            var today = new Date();
-
-            var first_day = (new Date(today.getFullYear(), today.getMonth(), 1)).getDay();
-            var first_date = (new Date(today.getFullYear(), today.getMonth(), 1));
-            var last_date = (new Date(today.getFullYear(), today.getMonth()+1, 0))
-            var last_day = last_date.getDay();
-            //document.querySelector('div.month').innerHTML=months[today.getMonth()]
-            //document.querySelector('span.year').innerHTML=today.getYear()
-            var monthTitle= createElement('div', document.querySelector('div.calendar-title'), 'month',months[new Date().getMonth()]);
-            createElement('span', monthTitle, 'year', new Date().getFullYear())
-
-            var last_month_date = new Date(today.getFullYear(), today.getMonth(),0).getDate();
+    var front_offset;
+    var back_offset;
+    var dayClicked =false
+    function createDaysArr(){
 
 
-            for(var i = 0, len = 6-first_day; i< len; i++){
-                month.unshift(new CustomDate(last_month_date-i, 0));
-            }
-            for(var i = 1, len =last_date.getDate(); i<=len; i++){
-                month.push(new CustomDate(i, 1, new Schedule(i, [])));
-            }
-            for(var i = 0, len = 6-last_day; i<len; i++){
-                month.push(new CustomDate((i+1), 2));
-            }
-            //console.log(month);
-            return month;
 
+
+        var today = new Date();
+
+        var first_day = (new Date(today.getFullYear(), today.getMonth(), 1)).getDay();
+        var first_date = (new Date(today.getFullYear(), today.getMonth(), 1));
+        var last_date = (new Date(today.getFullYear(), today.getMonth()+1, 0))
+        var last_day = last_date.getDay();
+        //document.querySelector('div.month').innerHTML=months[today.getMonth()]
+        //document.querySelector('span.year').innerHTML=today.getYear()
+         front_offset=first_day
+         back_offset=last_day
+
+        var monthTitle= createElement('div', document.querySelector('div.calendar-title'), 'month',months[new Date().getMonth()]);
+        createElement('span', monthTitle, 'year', new Date().getFullYear())
+
+        var last_month_date = new Date(today.getFullYear(), today.getMonth(),0).getDate();
+
+
+        for(var i = 0, len = 6-first_day; i< len; i++){
+            console.log('')
+            month.unshift(new CustomDate(last_month_date-i, 0));
         }
-
-        function createCalendar(){
-            var month = createDaysArr()
-            var cal_div = document.querySelector('div.calendar-days')
-            month.forEach(function(day){
-                var classname;
-                switch(day.monthType){
-                    case 0:
-                        classname='day prev-month'
-                        break;
-                    case 1:
-                        classname='day curr-month'
-                        break;
-                    case 2:
-                        classname='day next-month'
-                        break;
-                }
-                createElement('div', cal_div, classname,day.day)
-            })
-
+        for(var i = 1, len =last_date.getDate(); i<=len; i++){
+            month.push(new CustomDate(i, 1, new Schedule(i, [])));
         }
-        createCalendar();
-        function createElement(elementType, parent, className, innerHTML){
-            var element = document.createElement(elementType);
-            if(parent) parent.appendChild(element);
-            if(className) element.className=className;
-            if(innerHTML) element.innerHTML=innerHTML;
-            return element;
-
+        for(var i = 0, len = 6-last_day; i<len; i++){
+            month.push(new CustomDate((i+1), 2));
         }
+        //console.log(month);
+        return month;
 
+    }
 
-    function CustomDate(day, monthType, schedule){
-        this.day = day,
-        this.monthType = monthType,
-        this.schedule=schedule || []
+    function createCalendar(){
+        var month = createDaysArr()
+        var cal_div = document.querySelector('div.calendar-days')
+        month.forEach(function(day){
+            var classname;
+            switch(day.monthType){
+                case 0:
+                    classname='day prev-month'
+                    break;
+                case 1:
+                    classname='day curr-month'
+                    break;
+                case 2:
+                    classname='day next-month'
+                    break;
+            }
+            createElement('div', cal_div, classname,day.day)
+        })
+        return cal_div;
+
     }
 
 
-    function Appointment (){
+    var calendar_div = createCalendar();
+    var tableElem = document.querySelector('table.times');
+    var tablePos = createTable(tableElem);
+
+    function createTable(tableElem){
+
+
+        var count=-1;
+        for (var i =0; i <48; i++){
+            var mins;
+            var ampm;
+            if(i%2===0){
+                mins = '00'
+                count++
+            }
+            else{
+                mins = '30'
+            }
+
+            if (i<24){
+                ampm=' am'
+            }else{
+                ampm=' pm'
+            }
+           // console.log(count)
+            var tr =createElement('tr',tableElem, hours[count]+':' +mins+ ampm, '')
+            var td =createElement('td', tr, hours[count]+':' +mins+ ampm+ ' tdtime',hours[count]+':' +mins+ ampm)
+            td.style.width = '5em'
+            var tdapp =createElement('td', tr, hours[count]+':' +mins+ ampm+ ' td2','')
+            if(i==23){
+                count = -1;
+            }
+        }
+        return tableElem
+
+    }
+
+
+    var schedTitle= document.querySelector('div.schedule-title');
+
+
+    calendar_div.addEventListener('click', function(event){
+
+            console.log(event.target.classList)
+            console.log(event.target)
+            console.log(event.target.innerHTML)
+            console.log(typeof event.target.innerHTML)
+            if(event.target.classList.contains('curr-month')){
+                dayClicked=true
+                var obj = month[event.target.innerHTML]
+                schedTitle.innerHTML='Schedule for: ' + months[new Date().getMonth()] + ' ' + event.target.innerHTML + ' ' + new Date().getFullYear()
+                console.log(obj)
+                //while(document.querySelectorAll())
+
+            }
+
+
+    })
+
+
+
+
+
+    function createElement(elementType, parent, className, innerHTML){
+        var element = document.createElement(elementType);
+        if(parent) parent.appendChild(element);
+        if(className) element.className=className;
+        if(innerHTML) element.innerHTML=innerHTML;
+        return element;
+
+    }
+
+
+    function CustomDate(day, monthType, schedules){
+        this.day = day,
+        this.monthType = monthType,
+        this.schedules=schedules || []
+    }
+
+
+    function Appointment (start, end, content){
         this.start=start,
         this.end =end,
         this.content = content
@@ -86,36 +161,94 @@
         this.appointments =appointments || []
     }
 
-    var appdiv = document.querySelector('div.appointments')
+    var appdiv = document.querySelector('table.times')
 
     appdiv.addEventListener('mousedown', function(event){
-        var div =document.createElement('div');
-        div.className='rectangle';
-        appdiv.appendChild(div);
-        move.initPos=[event.clientX, event.clientY];
+
+        event.preventDefault()
+
+        //var div =document.createElement('input');
+        //div.className='rectangle';
+        //appdiv.appendChild(div);
+        if(dayClicked){
+            move.initPos=event.target.classList
 
 
-        appdiv.addEventListener('mousemove', move);
+            appdiv.addEventListener('mouseup', move);
+        }
 
 
     })
-        appdiv.addEventListener('mouseup', function(event){
-            appdiv.removeEventListener('mousemove', move);
-        });
-
-        function move(event){
-
-            var currPos = [event.clientX, event.clientY];
-
-            this.lastChild.style.width = Math.abs(currPos[0]-move.initPos[0]) + 'px';
-            this.lastChild.style.height = Math.abs(currPos[1]-move.initPos[1]) + 'px';
-            this.lastChild.style.left =move.initPos[0]<currPos[0]? (move.initPos[0]-appdiv.offsetLeft) + 'px':currPos[0]-appdiv.offsetLeft +'px' ;
-            this.lastChild.style.top = move.initPos[1]<currPos[1]? (move.initPos[1]) + 'px':currPos[1] +'px';
 
 
+   // appdiv.addEventListener('mouseup', move);
 
-
+    function move(event){
+        event.preventDefault()
+        var currPos = event.target.classList
+        var endTime = event.target.classList
+        var time = endTime[0]
+        var times= time.split(':')
+        var timeNums=[]
+        times.forEach(function(x){
+            timeNums.push(parseInt(x))
+        })
+        if(timeNums[1]==0){
+            timeNums[1]=30
+        }else{
+            if(timeNums[0] ===12){
+                timeNums[0]=1
+            }else{
+                timeNums[0]++
+            }
+            timeNums[1] = 0
         }
+
+        getAppointment(move.initPos, currPos, timeNums);
+
+    }
+
+
+    function getAppointment(start,endPos , endTimeArr){
+        var startTime=start[0]+ ' ' + start[1]
+       var endHr = endTimeArr[0]
+        var endMin = endTimeArr[1]
+        var ampm=endPos[1]
+
+        if(endHr ===12) {
+
+            if (ampm === 'am') {
+                ampm = 'pm'
+            }else{
+                ampm= 'am'
+            }
+        }
+
+
+        if (endHr<10){
+            endHr = '0'+endHr
+        }
+        else{
+            endHr = ''+endHr
+        }
+        if(endMin ===0){
+            endMin='00'
+        }else{
+            endMin='30'
+        }
+
+
+        var endTime = endHr+':'+endMin + ' ' + ampm
+        var appointmentContent= prompt('Create Appointment from ' + startTime + ' to ' + endTime+ ' for this day', '' )
+
+        console.log(appointmentContent)
+        if(appointmentContent){
+            var appointment = new Appointment(startTime, endTime,appointmentContent)
+            console.log(appointment)
+        }
+
+    }
+
 })
     /**
      * Created by adityamangipudi1 on 4/15/15.
